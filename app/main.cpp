e75f9ae5dbf7085f9a07a747acdbebdddaf4aa88
swagquotient0 <54880010+swagquotient0@users.noreply.github.com>
2019-10-19 17:35:24 -0400

    Editing and updating main.cpp
    2.2 - Reviewing, editing and updating main.cpp according to the requirements.

--- app/main.cpp
+++ app/main.cpp
@@ -4,17 +4,17 @@
 int main() {
 
   double requiredVel = 30;  // Velocity to be achieved
-  std::vector<double> requiredD { 100, 100 };
-  int requiredTheta = 50;  // Heading angle to be achieved
+  std::vector<double> requiredD { 50, 100 };
+  int requiredTheta = 80;  // Heading angle to be achieved
 
-  std::vector<double> currentD { 5.0, 5.0 };
+  std::vector<double> currentD { 15.0, 5.0 };
   std::vector<double> driveWheelVel;
   double steeringAng;
 
   AckermannController ackermann;
   ackermann.setL(2.4);  /// Wheel Base
   ackermann.setV(28);  //  current velocity
-  ackermann.setTheta(90);  // Current Orientation
+  ackermann.setTheta(30);  // Current Orientation
   ackermann.setD(currentD);  // Current Position
 
   std::cout << "Required Orientation : " << requiredTheta << std::endl;
@@ -23,8 +23,6 @@ int main() {
   steeringAng = ackermann.computeSteering(requiredD, requiredTheta);
 
   driveWheelVel = ackermann.driveVelocities(requiredVel, requiredTheta);
-
-  std::cout << "Current steering angle : " << steeringAng << std::endl;
   std::cout << "Required Velocity : " << requiredVel << std::endl;
   std::cout << "Drive Wheel Velocity 1 : " << driveWheelVel[0] << std::endl;
   std::cout << "Drive Wheel Velocity 2 : " << driveWheelVel[1] << std::endl;

073b085671c0a4f89e0346b57d9fdfec03f08ff6
swagquotient0 <54880010+swagquotient0@users.noreply.github.com>
2019-10-19 16:36:42 -0400

    Update main.cpp
    2.1 - Updated the main.cpp file with the code

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include <AckermannController.hpp>
 
-int main()
-{
-    return 0;
+int main() {
+
+  double requiredVel = 30;  // Velocity to be achieved
+  std::vector<double> requiredD { 100, 100 };
+  int requiredTheta = 50;  // Heading angle to be achieved
+
+  std::vector<double> currentD { 5.0, 5.0 };
+  std::vector<double> driveWheelVel;
+  double steeringAng;
+
+  AckermannController ackermann;
+  ackermann.setL(2.4);  /// Wheel Base
+  ackermann.setV(28);  //  current velocity
+  ackermann.setTheta(90);  // Current Orientation
+  ackermann.setD(currentD);  // Current Position
+
+  std::cout << "Required Orientation : " << requiredTheta << std::endl;
+  std::cout << "Current Orientation : " << ackermann.getTheta() << std::endl;
+
+  steeringAng = ackermann.computeSteering(requiredD, requiredTheta);
+
+//  driveWheelVel = ackermann.driveVelocities(requiredVel, requiredTheta);
+
+//  std::cout << "Current steering angle : " << steeringAng << std::endl;
+//  std::cout << "Required Velocity : " << requiredVel << std::endl;
+//  std::cout << "Drive Wheel Velocity 1 : " << driveWheelVel[0] << std::endl;
+//  std::cout << "Drive Wheel Velocity 2 : " << driveWheelVel[1] << std::endl;
+  return 0;
 }
